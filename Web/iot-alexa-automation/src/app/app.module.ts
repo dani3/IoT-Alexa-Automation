69032e41ea198f432a89947ce99cd35386ecec99
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2019-10-18 18:08:03 +0200

    Added sample angular project

---
+++ Web/iot-alexa-automation/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+@NgModule({
+  imports: [
+      MDBBootstrapModule.forRoot()
+  ]
+})
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
